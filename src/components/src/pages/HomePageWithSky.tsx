a565a0fc4f58fedf181f434d7fbab0571b01be3f
joy12000 <jeonguk1203@gmail.com>
2025-08-25 14:26:39 +0900

    Delete src/components/src directory

--- src/components/src/pages/HomePageWithSky.tsx
+++
@@ -1,6 +0,0 @@
-
-import React from "react";
-import HomeSky from "./HomeSky";
-
-/** Legacy entry wrapper to keep filename parity with older routes */
-export default function HomePageWithSky(){ return <HomeSky />; }
