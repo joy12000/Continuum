6e9180086ded9997ad2a85bb09c3cecbeec343f7
joy12000 <jeonguk1203@gmail.com>
2025-09-03 12:21:06 +0900

    fix: Add QueryClientProvider to fix app crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -7,10 +8,15 @@ import './styles/sky.css';
 import './styles/toast.css';
 import './styles/modal.css';
 import './styles/calendar.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
@@ -20,3 +26,4 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').catch(()=>{});
   });
 }
+
